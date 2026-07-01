75a561069d0c14fe149f0fe2d23ec74b5dba406b
Leandro Brito do Nascimento Nogueira <leandro.nogueira@gpnet.com.br>
2026-07-01 10:22:49 -0300

    ajustes de cors 2

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const frontendUrl = (process.env.FRONTEND_URL ?? 'http://localhost:8080').replace(/\/$/, '');
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'https://gestao-de-performance-liart.vercel.app/',
+    origin: frontendUrl,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
